7ea7911ce02a7c51670aae058e0a9ff94a366f05
Seth Nelson <seth.nelson@outlook.com>
2021-05-19 21:16:10 -0700

    added an array of arrays with indexes to display the shapes

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// // initialize grid for game board **optional
-// function createBoard() {
-//     var div = document.createElement('div');
-    
-//     div.style.display = 'flex';
-//     div.style.flexWrap = 'wrap';
-//     div.style.flexDirection = 'row';
-//     div.style.width = '200px';
-//     div.style.height = '400px';
-//     div.style.backgroundColor = 'grey';
-
-//     document.getElelentById('grid').appendChild(div);
-// }
-
-
 // initialize div's for game board
 function addDivs() {
     for (var i = 0; i < 200; i++) {
@@ -20,11 +5,73 @@ function addDivs() {
 
     div.style.width = '18px';
     div.style.height = '18px';
-    div.style.backgroundColor = 'teal';
-    div.style.opacity = '50%';
+    div.style.opacity = '0.75';
     div.style.border = '1px solid black';
 
-
-    document.getElementById('grid').appendChild(div);
+    document.querySelector('.grid').appendChild(div);
     }
 }
+
+
+// this logic takes all of the divs in the model and creates an array with assigned indexes
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('grid')
+    // .from method allows you to make an array from elements
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    // width of the grid used for transitions
+    const width = 10
+    console.log(squares)
+
+    const lShape = [
+        [1, 2, width + 1, width * 2 + 1],
+        [width, width + 1, width + 2],
+        [1, width + 1, width * 2 + 1],
+        [0, width, width + 1, width + 2],
+    ]
+
+    // const zShape = [
+    //     [1, 2, width + 1, width * 2 + 1],
+    //     [],
+    //     [],
+    //     [],
+    // ]
+
+    // const tShape = [
+    //     [1, 2, width + 1, width * 2 + 1],
+    //     [],
+    //     [],
+    //     [],
+    // ]
+
+    // const oShape = [
+    //     [1, 2, width + 1, width * 2 + 1],
+    //     [],
+    //     [],
+    //     [],
+    // ]
+
+    // const iShape = [
+    //     [1, 2, width + 1, width * 2 + 1],
+    //     [],
+    //     [],
+    //     [],
+    // ]
+
+
+    const shapes = [lShape]
+
+    // current board position start **includes the 9 total squares
+    let currentPosition = 3
+    let current = shapes[0][0]
+    let randomShape = Math.floor(Math.random() * shapes.length)
+    console.log(randomShape)
+
+    // draw the first shape's rotational location
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('shape')
+        })
+    }
+
+    draw()
+})
